441efcf6d5ce2f8f89d52e595cc98bc6b5c24ab4
Lali Martinez <martinezlali4@gmail.com>
2023-01-24 15:05:34 -0800

    Updated variable names for clarity.

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ async function clickStartAProject() {
     await page.goto('https://www.webstacks.com/');
     await page.waitForTimeout(3000);
 
-    const xButton = await page.$x('//*[@id="mutiny-preact-0df4392a-d9aa-4fe3-bede-8289ebaf933a"]/div/div[1]');
-    await xButton[0].click();
+    const closeSurveyButton = await page.$x('//*[@id="mutiny-preact-0df4392a-d9aa-4fe3-bede-8289ebaf933a"]/div/div[1]');
+    await closeSurveyButton[0].click();
     await page.waitForTimeout(2000);
 
-    const elements = await page.$x('//*[@id="c6c6bca8-8975-582f-9ef1-cbcedad10b40"]');
-    await elements[0].hover();
+    const startProjectButton = await page.$x('//*[@id="c6c6bca8-8975-582f-9ef1-cbcedad10b40"]');
+    await startProjectButton[0].hover();
     await page.waitForTimeout(3000);
 
-    await elements[0].click();
+    await startProjectButton[0].click();
     await page.waitForTimeout(5000);
 
     await browser.close();
